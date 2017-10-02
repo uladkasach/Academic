af9ae507c16403a05953982a93f22f481a03e74c
UladKasach <nappa300@gmail.com>
2017-10-02 00:28:13 -0400

    plot energies for bound particle with delta potentials, hw3 p3 quantum

--- Physics/Phys550/hw3/plot_3.py
+++ Physics/Phys550/hw3/plot_3.py
@@ -4,6 +4,8 @@ import scipy
 import scipy.optimize
 import numpy as np
 import matplotlib.pylab as plt;
+from scipy.optimize import brentq
+
 
 
 
@@ -14,44 +16,88 @@ h = 1; #4.1357 * 10**(-15); #eV s
 h_bar = h / (2 * np.pi);
 
 
-def func(k, ratio):
+def func(k, ratio, bool_full_value = True):
     gamma = alpha * ratio;
-    print(gamma);
-    mu = 2*m*alpha/h_bar**2;
+    #mu = 2*m*alpha/h_bar**2;
     mu = alpha;
     lam = gamma/alpha;
-    print("---");
+    #print("---");
+    #print("k->" + str(k));
     fun = (4*k**2 - 4*k*mu + mu**2*(1 + lam**2))
-    fun2 = np.exp(4*k*a)
-    fun3 = 0 - mu**2*(1 + lam**2);
+    fun2 = np.exp(-4*k*a) * mu**2*(1 + lam**2)
     
-    print("---");
-    print(fun);
-    print(fun2);
-    return(fun);
+    #print(fun);
+    #print(fun2);
+    #print("full : " + str(fun - fun2));
+    if(bool_full_value == True):
+        return fun - fun2;
+    else:
+        return [fun, fun2];
 
 
+
+def find_all_roots(f, a, b, pars=(), min_window=0.001):
+    try:
+        one_root = brentq(f, a, b, pars)
+        print "Root at %g in [%g,%g] interval" % (one_root, a, b)
+    except ValueError:
+        print "No root in [%g,%g] interval" % (a, b)
+        return [] # No root in the interval
+
+    if one_root-min_window>a:
+        lesser_roots = find_all_roots(f, a, one_root-min_window, pars)
+    else:
+        lesser_roots = []
+
+    if one_root+min_window<b:
+        greater_roots = find_all_roots(f, one_root+min_window, b, pars)
+    else:
+        greater_roots = []
+
+    return lesser_roots + [one_root] + greater_roots
 
 ########
-## roots not working
+## find roots
 ##########
-ratio = 5000000
-min = 1
-max = 10 # set max to some sufficiently large value
-#root = scipy.optimize.brentq(func, min, max, args = (ratio)) # args just supplies any extra
-                                                       # argument for the function that isn't the varied parameter
+if(True):
+    full_roots = [];
+    ratio_range = np.arange(0, 1, 0.001);
+    for ratio in ratio_range:
+        #ratio = 0.25
+        min = -1
+        max = 1 # set max to some sufficiently large value
+        #root = scipy.optimize.brentq(func, min, max, args = (ratio)) # args just supplies any extra
+                                                               # argument for the function that isn't the varied parameter
+        roots = find_all_roots(func, min, max, pars=(ratio))
+        
+        full_roots.append(roots[0]);
+        print(roots);
+        if(len(roots) > 1): 
+            print("MORE THAN ONE ROOT FOUND!!!!");
+            exit();
 
-k_range = np.arange(0, 1, 0.001);
-left_vals = [];
-right_vals = [];
-for k in k_range:    
-    val = (func(k, ratio))
-    print(val);
-    print(ratio);
-    left_vals.append(val);
+    plt.plot(ratio_range, full_roots);
+    plt.axis('tight')
+    plt.ylabel(" [2mE]^(1/2)/h_bar");
+    plt.xlabel(" gamma / alpha ");
+    plt.title("Ratio vs Energy of Solutions");
+    plt.draw()
+    plt.savefig("results_3/ratio_vs_energy.png")
 
 
-plt.plot(k_range, left_vals);
-plt.axis('tight')
-plt.draw()
-plt.show()
+if(False): ## plot function
+    k_range = np.arange(0, 1, 0.001);
+    left_vals = [];
+    right_vals = [];
+    for k in k_range:    
+        val, val2 = (func(k, ratio, bool_full_value = False))
+        left_vals.append(val);
+        right_vals.append(val2);
+
+
+    plt.plot(k_range, left_vals);
+    plt.plot(k_range, right_vals);
+    plt.axis('tight')
+    plt.title("gamma/alpha = " + str(ratio));
+    plt.draw()
+    plt.savefig("results_3/ratio_" + str(ratio)+".png")
