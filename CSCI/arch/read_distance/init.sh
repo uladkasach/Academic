ea4e5a629de4695fe341c576e0218a086002470e
UladKasach <nappa300@gmail.com>
2018-03-25 05:17:35 -0400

    move out the read_distance project into its own repo

--- CSCI/arch/read_distance/init.sh
+++
@@ -1,8 +0,0 @@
-## init arduino component
-cd src && \
-mkdir -p components && \
-cd components && \
-git clone https://github.com/espressif/arduino-esp32.git arduino && \
-cd arduino && \
-git submodule update --init --recursive && \
-cd ../.. &&
